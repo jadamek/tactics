964c7fb0dc42afad453bc9d67ef2e9b1852baf8c
Jordan <chaos_killa@live.com>
2016-06-02 03:09:11 -0400

    SpriteTile implemented, resource images added

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import sfml as sf
+from sprite.map.sprite_tile import SpriteTile
 
-window = sf.Window(sf.VideoMode(640, 480), 'Tactics!')
+window = sf.RenderWindow(sf.VideoMode(640, 480), 'Tactics!')
 window.framerate_limit = 60
+
+tile_texture = sf.Texture.from_file('resources/graphics/GrassTile.png')
+tile = SpriteTile(tile_texture)
+tile.move(sf.Vector2(100, 100))
+#tile.rotate(30)
+tile.reset_height(8)
+
+target = sf.Texture.from_file('resources/graphics/Target.png')
+cursor = sf.Sprite(target)
+cursor.move(sf.Vector2(92, 92))
+
+print str(tile.position)
 
 while window.is_open:
     for event in window.events:
-            if type(event) is sf.CloseEvent:
-                    window.close()
+        if type(event) is sf.CloseEvent:
+            window.close()
                     
-            window.active = True
-            window.display()
+    window.clear()
+    window.draw(tile)
+    window.draw(cursor)
+    window.display()
---
+++ sprite/map/sprite_tile.py
@@ -0,0 +1,83 @@
+import sfml as sf
+
+#================================================================================
+class SpriteTile(sf.TransformableDrawable):
+#================================================================================
+# A sprite of a single (continuous or discrete) tile column.
+#================================================================================
+# Methods
+    #----------------------------------------------------------------------------
+    # - Tile Sprite Contructor
+    #----------------------------------------------------------------------------
+    # * texture : the bitmap containing the image data for the tile.
+    # * width : width in pixels; 0 to fit automatically to texture
+    # * length : width in pixels; 0 to fit automatically to texture
+    # * height : height in pixels
+    # * continuous : TRUE if the tile's sprite continues infinitely downward
+    #----------------------------------------------------------------------------
+    def __init__(self, texture, width = 0, length = 0, height = 0, continous = False):
+        sf.TransformableDrawable.__init__(self)
+        
+        self.width = width
+        self.length = length
+        self.height = height
+        self.continuous = continous
+
+        if width <= 0:
+            self.width = texture.width / 2        
+        if length <= 0:
+            self.length = texture.height / 2
+        if height < 0:
+            self.height = 0
+                
+        texture.repeated = True        
+            
+        self.top_ = sf.Sprite(texture)
+        self.top_.texture_rectangle = sf.Rectangle((0, 0), (self.width, self.length))
+        self.top_.position = sf.Vector2(-1 * self.width / 2, -1 * self.length / 2 - self.height)
+        self.center_ = sf.Sprite(texture)        
+        self.center_.position = sf.Vector2(-1 * self.width / 2, -1 * self.height)
+
+        if not self.continuous:
+            self.bottom_ = sf.Sprite(texture)
+            self.bottom_.texture_rectangle = sf.Rectangle((0, self.length), (self.width, self.length))
+            self.bottom_.position = sf.Vector2(-1 * self.width / 2, -1 * self.length / 2)
+            self.center_.texture_rectangle = sf.Rectangle((self.width, 0), (self.width, self.height))
+        else:
+            self.center_.texture_rectangle = sf.Rectangle((self.width, 0), (self.width, texture.get_maximum_size()))
+            
+    #----------------------------------------------------------------------------
+    # - Reset Height
+    #----------------------------------------------------------------------------            
+    def reset_height(self, height):
+        self.height = height
+        if height < 0:
+            self.height = 0
+
+        self.top_.position = sf.Vector2(-1 * self.width / 2, -1 * self.length / 2 - self.height)
+        self.center_.position = sf.Vector2(-1 * self.width / 2, -1 * self.height)
+        
+        if not self.continuous:
+            self.center_.texture_rectangle = sf.Rectangle((self.width, 0), (self.width, self.height))
+
+    #----------------------------------------------------------------------------
+    # - Draw (Overload)
+    #----------------------------------------------------------------------------
+    def draw(self, target, states):
+        states.transform.combine(self.transform)
+        
+        if self.bottom_ is not None:
+            target.draw(self.bottom_, states)
+        target.draw(self.center_, states)
+        target.draw(self.top_, states)
+    
+# Members    
+    width = 0
+    length = 0
+    height = 0
+    continuous = False
+    
+    top_ = None
+    center_ = None
+    bottom_ = None
+#================================================================================
