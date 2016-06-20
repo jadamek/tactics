a996f865b603b084dac9a7c06c6958abfc2b1c4f
Jordan <chaos_killa@live.com>
2016-06-20 03:18:46 -0400

    Added global settings file. MobileObject.speed redefined.

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,7 @@ from map.test_tile import TestTiles
 from map.test_map import TestMap
 from container.test_zlist import TestZList
 from sprite.map.test_sprite_tile import TestSpriteTiles
+from animated.test_mobile_object import TestMobileObject
 
 if __name__ == "__main__":
 	unittest.main(verbosity = 2)
