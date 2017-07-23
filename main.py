af561d5d394497fe76d19898e73b09306471b6e0
Jordan <chaos_killa@live.com>
2017-07-23 15:48:56 -0400

    Semantic change to event handling in pySFML2 ...

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from sfml import sf
 from screen.view_ex import ViewEx
 import settings
 
+
+#===========================================================================
+# - Sprite Setup
+soul_texture = sf.Texture.from_file('resources/graphics/Soul.png')
+soul = sf.Sprite(soul_texture)
+
+#===========================================================================
+# - Window Setup
 window = sf.RenderWindow(sf.VideoMode(640, 480), 'Tactics!')
 window.framerate_limit = 60
 
 view = ViewEx(sf.Rect((-320, -240), (640, 480)))
 window.view = view
+
+view.fade_in(3)
 
 clock = sf.Clock()
 elapsed = 0.0
@@ -15,9 +25,9 @@ closing = False
 
 while window.is_open:
     for event in window.events:
-        if type(event) is sf.Event.CLOSED:
+        if event.type == sf.Event.CLOSED:
             window.close()
-
+    
     if sf.Keyboard.is_key_pressed(sf.Keyboard.LEFT):
         if sf.Keyboard.is_key_pressed(sf.Keyboard.L_SHIFT):
             if not view.scrolling():
@@ -44,9 +54,10 @@ while window.is_open:
 
     elif closing and not view.tinting():
         window.close()
-
+    
     elapsed = clock.restart().seconds
     view.update(elapsed)
     window.clear()
+    window.draw(soul)
     view.draw_overlays(window)
     window.display()
